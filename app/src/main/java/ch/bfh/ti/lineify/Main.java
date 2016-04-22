6920617baac26da70bb4f0e8dc1b671d865a3e8c
Benjamin Müller <bm@nexplore.ch>
2016-04-22 10:16:33 +0200

    Add rx java

--- app/src/main/java/ch/bfh/ti/lineify/Main.java
+++ app/src/main/java/ch/bfh/ti/lineify/Main.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 
+import rx.Observable;
+
 public class Main extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +15,15 @@ public class Main extends Activity {
 
         int flags = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
         findViewById(R.id.fullscreen_content).setSystemUiVisibility(flags);
+
+        Observable<String> helloMessagesObservable = Observable.create(subscriber -> {
+            subscriber.onNext("HI");
+            subscriber.onNext("HALLO");
+            subscriber.onNext("GRüEZI");
+            subscriber.onCompleted();
+        });
+
+        helloMessagesObservable.subscribe(s -> System.out.println(s));
     }
 
     @Override
