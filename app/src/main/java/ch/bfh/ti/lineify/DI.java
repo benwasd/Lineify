836c1c004334f89515d0370edd42354e976c9eb8
Benjamin Müller <bm@nexplore.ch>
2016-04-29 12:30:27 +0200

    Add Global DI class

---
+++ app/src/main/java/ch/bfh/ti/lineify/DI.java
@@ -0,0 +1,46 @@
+package ch.bfh.ti.lineify;
+
+import android.content.Context;
+
+import ch.bfh.ti.lineify.core.dependencyInjection.DependencyContainer;
+import ch.bfh.ti.lineify.core.dependencyInjection.IDependencyContainer;
+
+public class DI {
+    private static Object setupLockPad = new Object();
+    private static IDependencyContainer dependencyContainer;
+
+    public static IDependencyContainer container() {
+        if (!isAlreadySetUp()) {
+            throw new Error("Dependency container not set up.");
+        }
+
+        return dependencyContainer;
+    }
+
+    public static void setup(Context applicationContext) {
+        synchronized (setupLockPad) {
+            if (!isAlreadySetUp()) {
+                setupDependencyContainer();
+                setupApplicationContextInContainer(applicationContext);
+            }
+        }
+    }
+
+    public static boolean isAlreadySetUp() {
+        return dependencyContainer != null;
+    }
+
+    private static void setupDependencyContainer() {
+        IDependencyContainer container = new DependencyContainer();
+        ch.bfh.ti.lineify.core.Registry.initializeDependencies(container);
+        ch.bfh.ti.lineify.infrastructure.connectivity.Registry.initializeDependencies(container);
+        ch.bfh.ti.lineify.infrastructure.location.Registry.initializeDependencies(container);
+        ch.bfh.ti.lineify.ui.Registry.initializeDependencies(container);
+
+        dependencyContainer = container;
+    }
+
+    private static void setupApplicationContextInContainer(Context applicationContext) {
+        dependencyContainer.registerContainerControlled(Context.class, () -> applicationContext);
+    }
+}
--- app/src/main/java/ch/bfh/ti/lineify/ui/Main.java
+++ app/src/main/java/ch/bfh/ti/lineify/ui/Main.java
@@ -10,12 +10,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-import com.google.android.gms.location.LocationRequest;
-
+import ch.bfh.ti.lineify.DI;
 import ch.bfh.ti.lineify.R;
-import pl.charmas.android.reactivelocation.ReactiveLocationProvider;
-import rx.Observable;
-import rx.Subscription;
+import ch.bfh.ti.lineify.core.IWayPointService;
 
 public class Main extends Activity {
     private static final int MY_PERMISSIONS_REQUEST_LOCATION = 1337;
@@ -24,15 +21,12 @@ public class Main extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Bootstrapper bootstrapper = new Bootstrapper();
-        bootstrapper.setup(this.getApplicationContext());
+        DI.setup(this.getApplicationContext());
 
         setContentView(R.layout.activity_main);
 
         int flags = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
         findViewById(R.id.fullscreen_content).setSystemUiVisibility(flags);
-
-
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
@@ -53,22 +47,20 @@ public class Main extends Activity {
     }
 
     public void beginLocationShizzel() {
-        LocationRequest request = LocationRequest.create() //standard GMS LocationRequest
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                .setInterval(100);
+        this.beginLocationShizzel(DI.container().resolve(IWayPointService.class));
+    }
 
+    public void beginLocationShizzel(IWayPointService wayPointService) {
         int[] counter = new int[1];
 
-        ReactiveLocationProvider locationProvider = new ReactiveLocationProvider(this.getApplicationContext());
-        Subscription subscription = locationProvider.getUpdatedLocation(request)
-                .subscribe(location -> {
+        wayPointService.wayPointObservable()
+            .subscribe(wayPoint -> {
                     TextView output = (TextView)findViewById(R.id.fullscreen_content);
-                    output.setText("C: " + (counter[0]++) + "\nSIZZL: " + location.getAltitude());
+                    output.setText("C: " + (counter[0]++) + "\nSIZZL: " + wayPoint.altitude());
 
-                    Log.i("Main", "Accuracy " + location.getAccuracy());
-                    Log.i("Main", "Altitude " + location.getAltitude());
-                    Log.i("Main", "Latitude " + location.getLatitude());
-                    Log.i("Main", "Longitude " + location.getLongitude());
+                    Log.i("Main", "Altitude " + wayPoint.altitude());
+                    Log.i("Main", "Latitude " + wayPoint.latitude());
+                    Log.i("Main", "Longitude " + wayPoint.longitude());
                 });
     }
 
