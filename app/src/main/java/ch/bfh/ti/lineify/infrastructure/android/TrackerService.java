ccda6c426d1d423db1b325355fd776b380069af4
Marco Kilchhofer <marco@kilchhofer.info>
2016-05-13 13:24:18 +0200

    TrackerService now extend Service instead of IntentService

--- app/src/main/java/ch/bfh/ti/lineify/infrastructure/android/TrackerService.java
+++ app/src/main/java/ch/bfh/ti/lineify/infrastructure/android/TrackerService.java
@@ -1,7 +1,10 @@
 package ch.bfh.ti.lineify.infrastructure.android;
 
-import android.app.IntentService;
+import android.app.Service;
 import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
 import java.util.concurrent.TimeUnit;
 
@@ -9,25 +12,47 @@ import ch.bfh.ti.lineify.DI;
 import ch.bfh.ti.lineify.core.IWayPointService;
 import ch.bfh.ti.lineify.core.IWayPointStore;
 import ch.bfh.ti.lineify.core.model.Track;
+import rx.Subscription;
+import rx.subscriptions.CompositeSubscription;
 
-public class TrackerService extends IntentService {
+public class TrackerService extends Service {
     private final IWayPointService wayPointService;
     private final IWayPointStore wayPointStore;
+    private final CompositeSubscription subscriptions;
+    
 
     public TrackerService() {
-        super("TrackerService");
         this.wayPointService = DI.container().resolve(IWayPointService.class);
         this.wayPointStore = DI.container().resolve(IWayPointStore.class);
+        this.subscriptions = new CompositeSubscription();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 
     @Override
-    protected void onHandleIntent(Intent intent) {
-        this.wayPointService.wayPointObservable(new Track("benwasd@github", "Meine Wanderung"))
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i("TrackerService","onStartCommand");
+        Subscription localPersistSubscription = this.wayPointService.wayPointObservable(new Track("benwasd@github", "Meine Wanderung"))
                 .buffer(10, TimeUnit.SECONDS)
                 .doOnNext(bufferdWayPoints -> this.wayPointStore.persistWayPoints(bufferdWayPoints))
                 .subscribe();
 
-        rx.Observable.interval(1, TimeUnit.MINUTES)
+        Subscription syncSubscription = rx.Observable.interval(1, TimeUnit.MINUTES)
                 .subscribe(x -> this.wayPointStore.syncWithBackend());
+
+        this.subscriptions.add(localPersistSubscription);
+        this.subscriptions.add(syncSubscription);
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i("TrackerService","onDestroy");
+        this.subscriptions.unsubscribe();
     }
 }
