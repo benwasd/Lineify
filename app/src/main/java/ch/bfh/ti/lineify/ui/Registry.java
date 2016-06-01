11268900dd18d322a049197c2a54a9485f3ed231
benwasd <benimueller@outlook.com>
2016-06-02 00:27:39 +0200

    Fix build

--- app/src/main/java/ch/bfh/ti/lineify/ui/Registry.java
+++ app/src/main/java/ch/bfh/ti/lineify/ui/Registry.java
@@ -1,6 +1,7 @@
 package ch.bfh.ti.lineify.ui;
 
 import ch.bfh.ti.lineify.core.IWayPointRepository;
+import ch.bfh.ti.lineify.core.IWayPointStore;
 import ch.bfh.ti.lineify.core.dependencyInjection.IDependencyContainer;
 import ch.bfh.ti.lineify.ui.adapter.TrackRecyclerViewAdapter;
 import ch.bfh.ti.lineify.ui.fragments.HistoryFragment;
@@ -9,7 +10,7 @@ import ch.bfh.ti.lineify.ui.fragments.TrackingFragment;
 public class Registry {
     public static void initializeDependencies(IDependencyContainer container) {
         container.registerContainerControlled(TrackingFragment.class, () -> new TrackingFragment());
-        container.registerContainerControlled(HistoryFragment.class, () -> new HistoryFragment(container.resolve(IWayPointRepository.class), container.resolve(TrackRecyclerViewAdapter.class)));
+        container.registerContainerControlled(HistoryFragment.class, () -> new HistoryFragment(container.resolve(IWayPointRepository.class), container.resolve(IWayPointStore.class), container.resolve(TrackRecyclerViewAdapter.class)));
         container.registerPerResolve(TrackRecyclerViewAdapter.class, () -> new TrackRecyclerViewAdapter());
     }
 }
